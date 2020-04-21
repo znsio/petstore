e5abf5dd94c76197c5d6445ade4f2431af576cd8
Joel.Rosario <joel@xnsio.com>
2020-04-21 16:06:48 +0530

    Running the latest contract from petstore-contracts repo

--- src/test/java/com/petstore/test/PetStoreContractTest.java
+++ src/test/java/com/petstore/test/PetStoreContractTest.java
@@ -14,12 +14,27 @@ public class PetStoreContractTest extends QontractJUnitSupport {
 
     @BeforeAll
     public static void setUp() {
-        File contract = new File("contract/service.contract");
+        File contract = getContractFile();
+        System.out.println("Contract file path: " + contract.getAbsolutePath());
+
         System.setProperty("path", contract.getAbsolutePath());
         System.setProperty("host", "localhost");
         System.setProperty("port", "8080");
 
         context = SpringApplication.run(Application.class);
+    }
+
+    private static File getContractFile() {
+        if(inGithubCI()) {
+            String workspace = System.getenv("GITHUB_WORKSPACE");
+            return new File(workspace + "/contracts/example/petstore/1.contract");
+        } else {
+            return new File(System.getProperty("user.home") + "/.qontract/repos/petstore/repo/examples/petstore/1.contract");
+        }
+    }
+
+    private static boolean inGithubCI() {
+        return System.getenv("CI") == "true";
     }
 
     @AfterAll
