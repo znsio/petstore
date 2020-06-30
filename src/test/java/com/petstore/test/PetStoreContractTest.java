27bf55bd00febc5c3faa0d1d559caa2e298c24c8
Joel Rosario <cirquitz@gmail.com>
2020-06-30 13:51:51 +0530

    upgraded Qontract to 0.12.2

--- src/test/java/com/petstore/test/PetStoreContractTest.java
+++ src/test/java/com/petstore/test/PetStoreContractTest.java
@@ -12,6 +12,7 @@ import java.nio.file.Path;
 import java.nio.file.Paths;
 
 import static org.assertj.core.api.Assertions.fail;
+import static org.springframework.util.FileSystemUtils.deleteRecursively;
 import static run.qontract.core.ContractUtilities.getContractFileName;
 import static run.qontract.core.versioning.ContractIdentifierKt.contractNameToRelativePath;
 
@@ -20,33 +21,19 @@ public class PetStoreContractTest extends QontractJUnitSupport {
 
     @BeforeAll
     public static void setUp() {
-        String contractPath = getContractPath("run.qontract.examples.petstore", 1, 1);
-        System.out.println("Contract file path: " + contractPath);
-        System.setProperty("path", contractPath);
+        System.setProperty("manifestFile", "./manifest.json");
+
+        String workingDirectory = "./target/qontract";
+        File workingDirectoryFile = new java.io.File(workingDirectory);
+        if(workingDirectoryFile.exists())
+            deleteRecursively(workingDirectoryFile);
+
+        System.setProperty("workingDirectory", workingDirectory);
 
         System.setProperty("host", "localhost");
         System.setProperty("port", "8080");
 
         context = SpringApplication.run(Application.class);
-    }
-
-    private static String getContractPath(String name, Integer majorVersion, Integer minorVersion) {
-        if(inGithubCI()) {
-            String workspace = System.getenv("GITHUB_WORKSPACE");
-            String contractPath = workspace + File.separator + "contracts" + File.separator + contractNameToRelativePath(name);
-            return getContractFileName(new File(contractPath).getAbsolutePath(), majorVersion, minorVersion);
-        } else {
-            Path path = Paths.get(System.getProperty("user.home"), "contracts", "petstore-contracts", contractNameToRelativePath(name)).toAbsolutePath();
-            String contractPath = getContractFileName(new File(path.toString()).getAbsolutePath(), majorVersion, minorVersion);
-            if(contractPath == null)
-                fail("Contract must exist at path USER_HOME/contracts/petstore-contracts. Checkout https://github.com/qontract/petstore-contracts into USER_HOME/contracts.");
-
-            return contractPath;
-        }
-    }
-
-    private static boolean inGithubCI() {
-        return "true".equals(System.getenv("CI"));
     }
 
     @AfterAll
