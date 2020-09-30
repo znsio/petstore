fe86afe3a575a7f056689c3f77b58524363aaba7
Joel Rosario <cirquitz@gmail.com>
2020-09-30 16:34:18 +0530

    Updated to Qontract 0.18.0

--- src/test/java/com/petstore/test/PetStoreContractTest.java
+++ src/test/java/com/petstore/test/PetStoreContractTest.java
@@ -7,15 +7,6 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.context.ConfigurableApplicationContext;
 import run.qontract.test.QontractJUnitSupport;
 
-import java.io.File;
-import java.nio.file.Path;
-import java.nio.file.Paths;
-
-import static org.assertj.core.api.Assertions.fail;
-import static org.springframework.util.FileSystemUtils.deleteRecursively;
-import static run.qontract.core.ContractUtilities.getContractFileName;
-import static run.qontract.core.versioning.ContractIdentifierKt.contractNameToRelativePath;
-
 public class PetStoreContractTest extends QontractJUnitSupport {
     private static ConfigurableApplicationContext context;
 
